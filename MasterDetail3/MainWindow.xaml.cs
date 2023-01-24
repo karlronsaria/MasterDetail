f3b51f3bae70ed78eb50a0cc973bb15a69a3cb0f
karlronsaria <andrew.mikedaniels@gmail.com>
2023-01-23 22:40:45 -0800

    add: Home and About pages

--- MasterDetail3/MainWindow.xaml.cs
+++ MasterDetail3/MainWindow.xaml.cs
@@ -28,12 +28,102 @@ namespace MasterDetail3
     {
         public MainWindow()
         {
+            Title = "XAML Brewer WinUI 3 Master-Detail Sample";
             this.InitializeComponent();
         }
+    }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+    public sealed partial class MainWindow : INavigate
+    {
+        private void NavigationView_Loaded(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            SetCurrentNavigationViewItem(
+                GetNavigationViewItems(typeof(Views.HomePage))
+                    .FirstOrDefault()
+            );
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            SetCurrentNavigationViewItem(
+                args.SelectedItemContainer
+                as NavigationViewItem
+            );
+        }
+
+        public List<NavigationViewItem>
+        GetNavigationViewItems()
+        {
+            List<NavigationViewItem>
+            result = new();
+            var items = NavigationView
+                .MenuItems
+                .Select(i => (NavigationViewItem)i)
+                .ToList();
+            items.AddRange(
+                NavigationView
+                    .FooterMenuItems
+                    .Select(i => (NavigationViewItem)i)
+            );
+            result.AddRange(items);
+
+            foreach (NavigationViewItem mainItem in items)
+            {
+                result.AddRange(
+                    mainItem
+                        .MenuItems
+                        .Select(i => (NavigationViewItem)i)
+                );
+            }
+
+            return result;
+        }
+
+        public List<NavigationViewItem>
+        GetNavigationViewItems(Type type)
+        {
+            return GetNavigationViewItems()
+                .Where(i => i.Tag.ToString() == type.Name)
+                .ToList();
+        }
+
+        public
+        List<NavigationViewItem>
+        GetNavigationViewItems(Type type, string title)
+        {
+            return GetNavigationViewItems(type)
+                .Where(i => i.Content.ToString() == title)
+                .ToList();
+        }
+
+        public void
+        SetCurrentNavigationViewItem(NavigationViewItem item)
+        {
+            if (item == null)
+            {
+                return;
+            }
+
+            if (item.Tag == null)
+            {
+                return;
+            }
+
+            ContentFrame.Navigate(Type.GetType(item.Tag.ToString()));
+            NavigationView.Header = item.Content;
+            NavigationView.SelectedItem = item;
+        }
+
+        public NavigationViewItem
+        GetCurrentNavigationViewItem()
+        {
+            return NavigationView.SelectedItem
+                as NavigationViewItem;
+        }
+
+        public bool Navigate(Type sourcePageType)
+        {
+            return ContentFrame.Navigate(sourcePageType);
         }
     }
 }
